9d1578d23920d338e858a0fce11480e86dba2045
afkpavi <rupavi2012@gmail.com>
2024-07-14 03:23:54 +0530

    quiz app init

---
+++ Question.java
@@ -0,0 +1,26 @@
+public class Question {
+    private int id;
+    private String query;
+    private String[] options;
+    private int answer;
+
+    public Question(int id, String query, String[] options, int answer){
+        this.id = id;
+        this.query = query;
+        this.options = options;
+        this.answer = answer;
+    }
+
+    public String getQuestion(){
+        return this.query;
+    }
+    public void showOptions(){
+        for(String option: this.options){
+            System.out.println(option);
+        }
+    }
+
+    public boolean validateAnswer(int userAnswer){
+        return this.answer == userAnswer;
+    }
+}
